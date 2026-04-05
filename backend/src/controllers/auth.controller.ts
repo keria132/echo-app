cb88e5a49d4aaa9879d07ac56c9031c7ec1f83f0
keria132 <keria132@protonmail.com>
2026-04-06 01:18:55 +0300

    feat: add basic auth

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from 'express';
 import * as Sentry from '@sentry/node';
 import { signupSchema } from '../schemas/auth.schema.js';
 import User from '../models/User.js';
-import { ERROR_MESSAGES, SALT_ROUNDS } from '../constants.js';
+import { AUTH_COOKIE_NAME, ERROR_MESSAGES, SALT_ROUNDS } from '../constants.js';
 import bcrypt from 'bcryptjs';
 import { generateToken } from '../lib/utils.js';
 import z from 'zod';
@@ -49,7 +49,50 @@ export const signup = async (request: Request, response: Response) => {
     });
   } catch (error) {
     console.error(ERROR_MESSAGES.signupError + ': ', error);
+    Sentry.captureException(error);
 
     return response.status(500).json({ message: ERROR_MESSAGES.serverError });
   }
+};
+
+export const login = async (request: Request, response: Response) => {
+  const { email, password } = request.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return response.status(400).json({ message: ERROR_MESSAGES.invalidLogin });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return response.status(400).json({ message: ERROR_MESSAGES.invalidLogin });
+    }
+
+    generateToken(user._id.toString(), response);
+
+    return response.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileIcon: user.profileIcon,
+    });
+  } catch (error) {
+    console.error('Error in login controller: ', error);
+    Sentry.captureException(error);
+
+    return response.status(500).json({ message: ERROR_MESSAGES.serverError });
+  }
+};
+
+export const logout = (_request: Request, response: Response) => {
+  response.clearCookie(AUTH_COOKIE_NAME, {
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: process.env.NODE_ENV === 'production',
+  });
+
+  return response.status(200).json({ message: 'Logout successful!' });
 };
